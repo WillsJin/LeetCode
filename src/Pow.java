40e8c8e329479cef1f917b8fc4a007d288d09a5f
Wills <willsjin21@gmail.com>
2015-04-22 18:17:35 +0800

    Solved two problems, yesterday.

--- src/Pow.java
+++ src/Pow.java
@@ -1,6 +1,71 @@
 /**
  * Created by wills on 15/4/21.
+ *
+ * See https://leetcode.com/problems/powx-n/
+ *
+ * Author https://github.com/WillsJin
  */
 public class Pow
 {
+    private final int[] p = new int[]{0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384,
+            32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432,
+            67108864, 134217728, 268435456, 536870912, 1073741824};
+    public double pow(double x, int n) {
+
+        if (x == 1.0) {
+            return 1.0;
+        }
+        if (x == -1.0) {
+            if (n % 2 == 0) {
+                return 1.0;
+            } else {
+                return -1.0;
+            }
+        }
+        boolean lessThanZero = false;
+        if (n < 0) {
+            n = -n;
+            lessThanZero = true;
+        }
+        double[] results = new double[32];
+
+        int i = 0;
+        results[0] = 1;
+        results[1] = x;
+        while (p[i] < n) {
+            if (i > 1) {
+                results[i] = results[i - 1] * results[i - 1];
+            }
+            i++;
+            if (i == 32) {
+                i--;
+                break;
+            }
+        }
+        if (i != 0) {
+            if (n != p[i]) {
+                i--;
+            } else if (i != 1){
+                results[i] = results[i - 1] * results[i - 1];
+            }
+        }
+        double result = 1;
+        while (n != 0) {
+            result *= results[i];
+            n = n - p[i];
+            while (p[i] > n) {
+                i--;
+            }
+        }
+        if (lessThanZero) {
+            result = 1 / result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Pow solution = new Pow();
+        double result = solution.pow(8.95371, -1);
+        System.out.println(result);
+    }
 }
--- src/SpiralMatrix.java
+++ src/SpiralMatrix.java
@@ -1,6 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by wills on 15/4/21.
+ *
+ * See https://leetcode.com/problems/spiral-matrix/
+ *
+ * Author https://github.com/WillsJin
  */
 public class SpiralMatrix
 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> result = new ArrayList<Integer>();
+
+        int m = matrix.length;
+        if (m <= 0) {
+            return result;
+        }
+        int n = matrix[0].length;
+        boolean[][] flag = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                flag[i][j] = false;
+            }
+        }
+        int turn = 0;
+        int i = 0, j = 0;
+
+        result.add(matrix[0][0]);
+        flag[0][0] = true;
+
+        while (result.size() < m * n) {
+            int _i = 0, _j = 0;
+            switch (turn % 4) {
+                case 0 : _i = i; _j = j + 1; break;
+                case 1 : _i = i + 1; _j = j; break;
+                case 2 : _i = i; _j = j - 1; break;
+                case 3 : _i = i - 1; _j = j; break;
+            }
+
+            if (0 <= _i && _i < m && 0 <= _j && _j < n && !flag[_i][_j]) {
+                i = _i;
+                j = _j;
+                result.add(matrix[i][j]);
+                flag[i][j] = true;
+            } else {
+                turn++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        SpiralMatrix solution = new SpiralMatrix();
+        int[][] matrix = new int[][]{{ 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }};
+        List<Integer> result = solution.spiralOrder(matrix);
+        System.out.println(result);
+    }
 }
